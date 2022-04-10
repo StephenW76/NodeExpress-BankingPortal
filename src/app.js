7492e805d0c5e71401be02d741c0e45a78fbb715
StephenW76 <103340339+StephenW76@users.noreply.github.com>
2022-04-10 02:37:40 -0400

    Part 3

--- src/app.js
+++ src/app.js
@@ -9,13 +9,37 @@ const app = express();
 app.set('views',path.join(__dirname+"/views"));
 app.set("view engine", 'ejs');
 
+
 app.use(express.static(path.join(__dirname+"/public")));
 
+const accountData = fs.readFileSync(path.join(__dirname+'/json/accounts.json'), {encoding: 'UTF8'});
+
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync(path.join(__dirname+'/json/users.json'), {encoding: 'UTF8'});
+
+const users = JSON.parse(userData);
+
+
 app.get("/", (req, res) => {
-    res.render("index", {title: "Index"});
-})
+    res.render("index", {title: "Account Summary", accounts: accounts});
+});
+
+app.get("/profile", (req, res) => {
+    res.render("profile", {user: users[0]});
+});
+
+app.get("/savings", (req, res) => {
+    res.render("account", {accounts: accounts.savings});
+});
+app.get("/credit", (req, res) => {
+    res.render("account", {accounts: accounts.credit});
+});
+app.get("/checking", (req, res) => {
+    res.render("account", {taccounts: accounts.checking});
+});
 
 app.listen(3000, ()=> {
     console.log('PS Project Running on port 3000!');
-})
+});
 
